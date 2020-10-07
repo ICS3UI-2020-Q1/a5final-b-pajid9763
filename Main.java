fe5da78a10117b8ee452cde3a2d139ffd3da81b6
pajid9763 <pajid9763@wrdsb.ca>
2020-10-07 15:39:40 +0000

    finished program that adds odd numbers to total

--- Main.java
+++ Main.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * program that adds odd numbers
+ * 
+ * @author Darian
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a number
+    System.out.println("Please enter the maximum number to sum to:");
+    int num = input.nextInt();
+
+    // declare variable to add odd numbers to
+    int total = 0;
+
+    // loop body will repeat based of these instructions
+    for (int i = 1; i <= num; i++) {
+
+      // if number is odd, it will be added to total
+      if (i % 2 != 0) {
+        total += i;
+
+        // if number is even, nothing happens and the loop body repeats
+      } else if (i % 2 == 0) {
+
+      }
+    }
+    // once for loop conditions are met, tell the user the sum of the odd numbers
+    System.out.println("The sum of the odd numbers is " + total);
+
   }
 }
